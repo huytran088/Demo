58d1ac1ea407579a7973d3e5ce82a6420da1d8c8
ghost <caohd@miamioh.edu>
2023-02-04 19:01:01 -0500

    movement + swing

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+
+    public Transform player;
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        Vector3 position = player.transform.position + new Vector3(0, 0.2f, 0);
+        transform.position = position + new Vector3(0, 0, -5);
+    }
+    
+}
